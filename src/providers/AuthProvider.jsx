79238be055e84fe98743ab36e4bb07397bface86
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-08 21:16:26 +0600

    Using loading state in auth to solve reload problem

--- src/providers/AuthProvider.jsx
+++ src/providers/AuthProvider.jsx
@@ -14,22 +14,27 @@ const auth = getAuth(app);
 
 const AuthProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const createUser = (email, password) => {
+    setLoading(true);
     return createUserWithEmailAndPassword(auth, email, password);
   };
 
   const signIn = (email, password) => {
+    setLoading(true);
     return signInWithEmailAndPassword(auth, email, password);
-  }
+  };
 
   const logOut = () => {
+    setLoading(true);
     return signOut(auth);
-  }
+  };
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setLoading(false);
     });
 
     return () => {
@@ -41,7 +46,8 @@ const AuthProvider = ({ children }) => {
     user,
     createUser,
     signIn,
-    logOut
+    logOut,
+    loading,
   };
 
   return (
--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -3,7 +3,11 @@ import { AuthContext } from "../providers/AuthProvider";
 import { Navigate } from "react-router-dom";
 
 const PrivateRoute = ({ children }) => {
-  const { user } = useContext(AuthContext);
+  const { user, loading } = useContext(AuthContext);
+
+  if (loading) {
+    return <span className="loading loading-dots loading-lg"></span>;
+  }
 
   if (user) {
     return children;
