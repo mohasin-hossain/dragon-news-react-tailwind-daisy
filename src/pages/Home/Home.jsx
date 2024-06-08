ef3b556125390638047e9e058c21fea4287f1440
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-08 12:19:28 +0600

    Custom Font

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,10 +1,14 @@
+import Header from "../Shared/Header/Header";
+import Navbar from "../Shared/Navbar/Navbar";
 
 const Home = () => {
-    return (
-        <div>
-            <h1>This is Home</h1>
-        </div>
-    );
+  return (
+    <div>
+      <Header></Header>
+      <Navbar></Navbar>
+      <h1>This is Home</h1>
+    </div>
+  );
 };
 
 export default Home;
