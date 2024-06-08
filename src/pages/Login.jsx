384b932307c164b11eb7cd4adf193eb893698757
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-08 19:24:32 +0600

    Login and Register page design

---
+++ src/pages/Login.jsx
@@ -0,0 +1,64 @@
+import { Link } from "react-router-dom";
+import Navbar from "./Shared/Navbar/Navbar";
+
+const Login = () => {
+  const handleLogin = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    console.log(form.get("email"));
+  };
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div>
+        <h1 className="text-center text-3xl mt-8">Please Login</h1>
+        <form
+          onSubmit={handleLogin}
+          className="card-body w-3/4 lg:w-1/2 mx-auto"
+        >
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+        <p className="text-center">
+          Do not have an Account? Please {" "}
+          <Link className="link link-primary font-bold" to="/register">
+            Register
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
---
+++ src/pages/Register.jsx
@@ -0,0 +1,88 @@
+import { Link } from "react-router-dom";
+import Navbar from "./Shared/Navbar/Navbar";
+
+const Register = () => {
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const form = new FormData(e.currentTarget);
+    console.log(form.get("email"));
+  };
+
+  return (
+    <div>
+      <Navbar></Navbar>
+      <div className="mb-8">
+        <h1 className="text-center text-3xl mt-2">Please Register</h1>
+        <form
+          onSubmit={handleRegister}
+          className="card-body w-3/4 lg:w-1/2 mx-auto"
+        >
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Name</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Name"
+              name="name"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Photo URL</span>
+            </label>
+            <input
+              type="text"
+              placeholder="Photo URL"
+              name="photoURL"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Email</span>
+            </label>
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              className="input input-bordered"
+              required
+            />
+          </div>
+          <div className="form-control">
+            <label className="label">
+              <span className="label-text">Password</span>
+            </label>
+            <input
+              type="password"
+              placeholder="Password"
+              name="password"
+              className="input input-bordered"
+              required
+            />
+            <label className="label">
+              <a href="#" className="label-text-alt link link-hover">
+                Forgot password?
+              </a>
+            </label>
+          </div>
+          <div className="form-control mt-6">
+            <button className="btn btn-primary">Login</button>
+          </div>
+        </form>
+        <p className="text-center">
+          Already have an Account? Please{" "}
+          <Link className="link link-primary font-bold" to="/login">
+            Login
+          </Link>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Register;
