60951e211ae5512ada54a6c66a3181c5f3a52bfc
Mohasin Hossain <mohasinhossainrajib2@gmail.com>
2024-06-08 13:56:32 +0600

    Left side nav

--- src/pages/Shared/LeftSideNav/LeftSideNav.jsx
+++ src/pages/Shared/LeftSideNav/LeftSideNav.jsx
@@ -1,10 +1,29 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
 const LeftSideNav = () => {
-    return (
-        <div>
-            <h1>Left Side</h1>
-        </div>
-    );
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    fetch("categories.json")
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return (
+    <div className="space-y-6">
+      <h1 className="text-2xl mb-4">All Categories</h1>
+      {categories.map((category) => (
+        <Link
+          key={category.id}
+          className="block px-4 text-base font-semibold"
+          to={`/category/${category.id}`}
+        >
+          {category.name}
+        </Link>
+      ))}
+    </div>
+  );
 };
 
 export default LeftSideNav;
